29c06c7b2c135a24be6e939b379980929c12f34c
mrsaiful778 <mrsaifulislam778@gmail.com>
2023-11-03 23:29:56 +0600

    first commit

---
+++ _jwt_step.js
@@ -0,0 +1,16 @@
+/***
+ * install jsonwetoken JWT 
+ * jwt.sign (payload, secret {expiresIn:})
+ * token client
+ * 
+ */
+/***
+ * how to store in the client site
+ * memory --> ok type
+ * local storage --> ok type (xss)
+ * cookies http only
+ * 
+ */
+/**
+ * set cookies with http only
+ */
